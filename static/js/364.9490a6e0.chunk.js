944124ed1dc8d7062229b0dbf3a2a23add8ae048
Pramitha Jayasooriya <lpramithamj@gmail.com>
2024-02-20 01:26:12 +0530

    Updates

---
+++ static/js/364.9490a6e0.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_portfolio=self.webpackChunkreact_portfolio||[]).push([[364],{4778:function(n,e,r){r.r(e),r.d(e,{default:function(){return o}});var t=r(4306),c=r(2791),i=r(184),u=function(){return(0,i.jsx)(t.YI,{children:(0,i.jsx)(t.WD,{name:"App",children:(0,i.jsx)(t.wA,{name:"Loader",children:(0,i.jsx)(t.$e,{children:(0,i.jsx)(a,{})})})})})};function a(){return(0,i.jsx)(c.Fragment,{children:"Loading..."})}var o=function(){return(0,i.jsx)(t.Vt,{palette:(0,i.jsx)(u,{})})}}}]);
+//# sourceMappingURL=364.9490a6e0.chunk.js.map
