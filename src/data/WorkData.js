73fbafe6eea24d08943b94a47ee87d8e841bd7a3
Pramitha Jayasooriya <lpramithamj@gmail.com>
2024-05-15 05:49:08 +0530

    update projects

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -1,163 +1,197 @@
 // work data
 
-import Ml from "../assets/Images/Ml.png"
-import LifePill from "../assets/Images/LifePill.png"
-import Ar from "../assets/Images/Ar.png"
-import Ft from "../assets/Images/Ft.png"
-import POS from "../assets/Images/POS.png"
-import SM from "../assets/Images/SM.png"
-import Vt from "../assets/Images/Vt.png"
-import Ga from "../assets/Images/Ga.png"
-import Tc from "../assets/Images/Tc.png"
-import Lt from "../assets/Images/Lt.png"
-import Cb from "../assets/Images/CB.png"
-import Bs from "../assets/Images/Bs.png"
-import Kc from "../assets/Images/Kc.png"
-import Bg from "../assets/Images/Bg.png"
-
+import Ml from "../assets/Images/Ml.png";
+import LifePill from "../assets/Images/LifePill.png";
+import Ar from "../assets/Images/Ar.png";
+import Ft from "../assets/Images/Ft.png";
+import POS from "../assets/Images/POS.png";
+import SM from "../assets/Images/SM.png";
+import Vt from "../assets/Images/Vt.png";
+import Ga from "../assets/Images/Ga.png";
+import Tc from "../assets/Images/Tc.png";
+import Lt from "../assets/Images/Lt.png";
+import Cb from "../assets/Images/CB.png";
+import Bs from "../assets/Images/Bs.png";
+import Kc from "../assets/Images/Kc.png";
+import Bg from "../assets/Images/Bg.png";
+import Omi from "../assets/Images/omi.png";
 
 export const Work = [
-    {
-        id:1,
-        name:"LifePill",
-        image:LifePill,
-        description:"LifePill is a software application designed to address the challenges of finding rare medicines and managing prescriptions efficiently. ",
-        tags:["Java","Spring Boot","PostgreSQL","Microservices","ReactJS"],
-        
-        demo:"https://github.com/Life-Pill/Life-Pill",
-        github:"https://github.com/Life-Pill"
-    },
-    {
-        id:2,
-        name:"AR Visionary Explorer",
-        image: Ar,
-        description:"AR-Visionary Explora is a revolutionary mobile application designed to enhance the furniture e-commerce experience through the integration of augmented reality (AR) technology. This Flutter-based app empowers users to visualize and assess furniture items in their living spaces before making a purchase, revolutionizing the traditional furniture buying process.",
-        tags:["Flutter","Firebase","ARCore","Dart"],
-        demo:"https://www.youtube.com/watch?v=-EUqf6oSlH0&t=80s",
-        github:"https://github.com/PramithaMJ/AR_Visionary_Explora.git"
-        
-    },
-    {
-        id:3,
-        name:"Personal Finance Tracker Web Application",
-        image:Ft,
-        description:"This web application designed to help you track personal finances." +
-            " You can add expenses and income, manage your money, and gain insights from the data and visualizing. ",
-        tags:["react","api","sass"],
-        
-        demo:"https://github.com/PramithaMJ/Personal_Financial_Tracker_Web_App.git",
-        github:"https://github.com/PramithaMJ/Personal_Financial_Tracker_Web_App.git"
-    },
-    {
-        id:4,
-        name:"Point of sale Desktop Application ",
-        image:POS,
-        description:"The Point of Sale (POS) System is a user-friendly application developed using WPF (Windows Presentation Foundation) and C#. It provides an efficient and intuitive interface for managing sales and performing various operations related to product management and user roles.",
-        tags:["C#","DOTNet","WPF","MVVM Architecture"],
-        
-        demo:"https://github.com/PramithaMJ/GroupProject_POS_3990_4040.git",
-        github:"https://github.com/PramithaMJ/GroupProject_POS_3990_4040.git"
-       
-    },
-    {
-        id:5,
-        name:"Predicting Loan Repayment Risk Detection",
-        image:Ml,
-        description:"Prediction the Loan Repayment Risk",
-        tags:["flutter","firebase","Dart","Google Maps","Geolocation","Realtime Database"],
+  {
+    id: 1,
+    name: "LifePill",
+    image: LifePill,
+    description:
+      "LifePill is a software application designed to address the challenges of finding rare medicines and managing prescriptions efficiently. ",
+    tags: ["Java", "Spring Boot", "PostgreSQL", "Microservices", "ReactJS"],
 
-        demo:"https://github.com/PramithaMJ/Predicting-Loan-Repayment-Risk-Detection.git",
-        github:"https://github.com/PramithaMJ/Predicting-Loan-Repayment-Risk-Detection.git"
+    demo: "https://github.com/Life-Pill/Life-Pill",
+    github: "https://github.com/Life-Pill",
+  },
+  {
+    id: 2,
+    name: "AR Visionary Explorer",
+    image: Ar,
+    description:
+      "AR-Visionary Explora is a revolutionary mobile application designed to enhance the furniture e-commerce experience through the integration of augmented reality (AR) technology. This Flutter-based app empowers users to visualize and assess furniture items in their living spaces before making a purchase, revolutionizing the traditional furniture buying process.",
+    tags: ["Flutter", "Firebase", "ARCore", "Dart"],
+    demo: "https://www.youtube.com/watch?v=-EUqf6oSlH0&t=80s",
+    github: "https://github.com/PramithaMJ/AR_Visionary_Explora.git",
+  },
+  {
+    id: 3,
+    name: "Personal Finance Tracker Web Application",
+    image: Ft,
+    description:
+      "This web application designed to help you track personal finances." +
+      " You can add expenses and income, manage your money, and gain insights from the data and visualizing. ",
+    tags: ["react", "api", "sass"],
 
-    },
-    {
-        id:6,
-        name:"Vehicle tracking Application",
-        image:Vt,
-        description:"Tracking vehicle realtime location and get the location of the vehicle using google maps.",
-        tags:["flutter","firebase","Dart","Google Maps","Geolocation","Realtime Database"],
-        
-        demo:"http://react-redux-todo-app-git-main-codebucks27.vercel.app/",
-        github:"https://github.com/codebucks27/React-Redux-Todo-App"
-        
-    },
-    {
-        id:7,
-        name:"Grocessary Application",
-        image:Ga,
-        description:"Grocery Application is a mobile application developed using Flutter. It is a simple application that allows users to add, update, and delete grocery items. Buy and sell grocery items.",
-        tags:["flutter","firebase","Dart","Stripe"],
-        demo:"http://react-redux-todo-app-git-main-codebucks27.vercel.app/",
-        github:"https://github.com/codebucks27/React-Redux-Todo-App"
-        
-    },
-    {
-        id:8,
-        name:"TickTockClone",
-        image:Tc,
-        description:"TickTockClone is a Kotlin-based mobile application that replicates the core features of the popular social media platform TikTok. The app allows users to create, share, and view short videos, as well as interact with other users through likes, comments, and follows.",
-        tags:["#Kotliune","JetPackcompose"],
-        demo:"https://github.com/PramithaMJ/TikTokClone.git",
-        github:"https://github.com/PramithaMJ/TikTokClone.git"
-        
-    },
-    {
-        id:9,
-        name:"Student Management System",
-        image:SM,
-        description:"The Student Management System is a desktop application developed using WPF (Windows Presentation Foundation). Basic CRUD operations are available in this desktop application to maintain a Student Management System.",
-        tags:["C#","WPF","MVVM Architecture"],
-        demo:"https://github.com/PramithaMJ/Student_Management_Stystem_3990.git",
-        github:"https://github.com/PramithaMJ/Student_Management_Stystem_3990.git"
-        
-    },
-    {
-        id:10,
-        name:"Canteen Management System",
-        image:Cb,
-        description:"Console base application using datastucture and algorithm to manage the canteen system.",
-        tags:["LinkedList","Stack","Queue","Hash Table","C++","Data Structure","Algorithm"],
-        demo:"https://github.com/PramithaMJ/CanteenManagementSystem.git",
-        github:"https://github.com/PramithaMJ/CanteenManagementSystem.git"
-    },
-    {
-        id:11,
-        name:"Bank Service",
-        image:Bs,
-        description:"Bank Application Backend by using Spring Boot with microservices architecture.",
-        tags:["Java","SpringBoot","Microservices"],
-        demo:"https://github.com/PramithaMJ/Student_Management_Stystem_3990.git",
-        github:"https://github.com/PramithaMJ/Student_Management_Stystem_3990.git"
-        
-    },
-    {
-        id:12,
-        name:"kids Clothes shoppping Web Application",
-        image:Kc,
-        description:"This web application designed to purchase Clothes.",
-        tags:["MERN","JavaScript"],
-        demo:"https://github.com/PramithaMJ/kids-Cloothes-app.git",
-        github:"https://github.com/PramithaMJ/kids-Cloothes-app.git"
-        
-    },
-    {
-        id:13,
-        name:"Blogger Application",
-        image:Bg,
-        description:"This web application put Blog.",
-        tags:["NextJS","JavaScript"],
-        demo:"https://github.com/PramithaMJ/nextJs_blogger_App.git",
-        github:"https://github.com/PramithaMJ/nextJs_blogger_App.git"
-        
-    },
-     {
-        id:13,
-        name:"Quiz App Backend",
-        image:Cb,
-        description:"This Spring boot by using microservices architecture to manage the quiz app.",
-        tags:["Java","SpringBoot","Microservices"],
-        demo:"https://github.com/PramithaMJ/Spring-Microservises.git",
-        github:"https://github.com/PramithaMJ/Spring-Microservises.git"
-        
-    },
-] 
+    demo: "https://github.com/PramithaMJ/Personal_Financial_Tracker_Web_App.git",
+    github:
+      "https://github.com/PramithaMJ/Personal_Financial_Tracker_Web_App.git",
+  },
+  {
+    id: 4,
+    name: "Point of sale Desktop Application ",
+    image: POS,
+    description:
+      "The Point of Sale (POS) System is a user-friendly application developed using WPF (Windows Presentation Foundation) and C#. It provides an efficient and intuitive interface for managing sales and performing various operations related to product management and user roles.",
+    tags: ["C#", "DOTNet", "WPF", "MVVM Architecture"],
+
+    demo: "https://github.com/PramithaMJ/GroupProject_POS_3990_4040.git",
+    github: "https://github.com/PramithaMJ/GroupProject_POS_3990_4040.git",
+  },
+  {
+    id: 5,
+    name: "Predicting Loan Repayment Risk Detection",
+    image: Ml,
+    description: "Prediction the Loan Repayment Risk",
+    tags: [
+      "flutter",
+      "firebase",
+      "Dart",
+      "Google Maps",
+      "Geolocation",
+      "Realtime Database",
+    ],
+
+    demo: "https://github.com/PramithaMJ/Predicting-Loan-Repayment-Risk-Detection.git",
+    github:
+      "https://github.com/PramithaMJ/Predicting-Loan-Repayment-Risk-Detection.git",
+  },
+  {
+    id: 6,
+    name: "Vehicle tracking Application",
+    image: Vt,
+    description:
+      "Tracking vehicle realtime location and get the location of the vehicle using google maps.",
+    tags: [
+      "flutter",
+      "firebase",
+      "Dart",
+      "Google Maps",
+      "Geolocation",
+      "Realtime Database",
+    ],
+
+    demo: "http://react-redux-todo-app-git-main-codebucks27.vercel.app/",
+    github: "https://github.com/codebucks27/React-Redux-Todo-App",
+  },
+  {
+    id: 7,
+    name: "Grocessary Application",
+    image: Ga,
+    description:
+      "Grocery Application is a mobile application developed using Flutter. It is a simple application that allows users to add, update, and delete grocery items. Buy and sell grocery items.",
+    tags: ["flutter", "firebase", "Dart", "Stripe"],
+    demo: "http://react-redux-todo-app-git-main-codebucks27.vercel.app/",
+    github: "https://github.com/codebucks27/React-Redux-Todo-App",
+  },
+  {
+    id: 8,
+    name: "TickTockClone",
+    image: Tc,
+    description:
+      "TickTockClone is a Kotlin-based mobile application that replicates the core features of the popular social media platform TikTok. The app allows users to create, share, and view short videos, as well as interact with other users through likes, comments, and follows.",
+    tags: ["#Kotliune", "JetPackcompose"],
+    demo: "https://github.com/PramithaMJ/TikTokClone.git",
+    github: "https://github.com/PramithaMJ/TikTokClone.git",
+  },
+  {
+    id: 9,
+    name: "Student Management System",
+    image: SM,
+    description:
+      "The Student Management System is a desktop application developed using WPF (Windows Presentation Foundation). Basic CRUD operations are available in this desktop application to maintain a Student Management System.",
+    tags: ["C#", "WPF", "MVVM Architecture"],
+    demo: "https://github.com/PramithaMJ/Student_Management_Stystem_3990.git",
+    github: "https://github.com/PramithaMJ/Student_Management_Stystem_3990.git",
+  },
+  {
+    id: 10,
+    name: "Canteen Management System",
+    image: Cb,
+    description:
+      "Console base application using datastucture and algorithm to manage the canteen system.",
+    tags: [
+      "LinkedList",
+      "Stack",
+      "Queue",
+      "Hash Table",
+      "C++",
+      "Data Structure",
+      "Algorithm",
+    ],
+    demo: "https://github.com/PramithaMJ/CanteenManagementSystem.git",
+    github: "https://github.com/PramithaMJ/CanteenManagementSystem.git",
+  },
+  {
+    id: 11,
+    name: "Bank Service",
+    image: Bs,
+    description:
+      "Bank Application Backend by using Spring Boot with microservices architecture.",
+    tags: ["Java", "SpringBoot", "Microservices"],
+    demo: "https://github.com/PramithaMJ/Student_Management_Stystem_3990.git",
+    github: "https://github.com/PramithaMJ/Student_Management_Stystem_3990.git",
+  },
+  {
+    id: 12,
+    name: "kids Clothes shoppping Web Application",
+    image: Kc,
+    description: "This web application designed to purchase Clothes.",
+    tags: ["MERN", "JavaScript"],
+    demo: "https://github.com/PramithaMJ/kids-Cloothes-app.git",
+    github: "https://github.com/PramithaMJ/kids-Cloothes-app.git",
+  },
+  {
+    id: 13,
+    name: "Blogger Application",
+    image: Bg,
+    description: "This web application put Blog.",
+    tags: ["NextJS", "JavaScript"],
+    demo: "https://github.com/PramithaMJ/nextJs_blogger_App.git",
+    github: "https://github.com/PramithaMJ/nextJs_blogger_App.git",
+  },
+  {
+    id: 13,
+    name: "Quiz App Backend",
+    image: Cb,
+    description:
+      "This Spring boot by using microservices architecture to manage the quiz app.",
+    tags: ["Java", "SpringBoot", "Microservices"],
+    demo: "https://github.com/PramithaMJ/Spring-Microservises.git",
+    github: "https://github.com/PramithaMJ/Spring-Microservises.git",
+  },
+  {
+    id: 14,
+    name: "Omi Game Network",
+    image: Omi,
+    description:
+      "This Network game by using Java threads and socket programming.",
+    tags: ["Java", "Threads", "Socket Programming"],
+    demo: "https://github.com/PramithaMJ/Omi-Game.git",
+    github: "https://github.com/PramithaMJ/Omi-Game.git",
+  },
+];
