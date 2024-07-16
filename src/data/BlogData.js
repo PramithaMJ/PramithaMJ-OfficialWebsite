fbf9124051bfb4aee8ba6fc8983503b5837f029f
Pramitha Jayasooriya <lpramithamj@gmail.com>
2024-07-16 14:15:30 +0530

    blogs

--- src/data/BlogData.js
+++ src/data/BlogData.js
@@ -1,8 +1,16 @@
 // blog data
 
 export const Blogs = [
-      {
+    {
         id:1,
+        name:"Trace the Path: Distributed Tracing with Zipkin in Microservices-1",
+        tags:["Microservices","Distribute Tracer","Zipkin","Java"],
+        date:"31 May, 2024",
+        imgSrc:"https://miro.medium.com/v2/resize:fit:1400/format:webp/1*I61IrtPF9joECTUW7YJm8g.png",
+        link:"https://medium.com/@lpramithamj/trace-the-path-distributed-tracing-with-zipkin-in-microservices-1-5096aaade124"
+    },
+      {
+        id:2,
         name:"Why do we need to use Circuit Bracker Pattern inside Microservices?",
         tags:["Microservices","Circuit Bracker","Design patterns","Java"],
         date:"7 April, 2024",
@@ -10,7 +18,7 @@ export const Blogs = [
         link:"https://medium.com/@lpramithamj/why-do-we-need-to-use-circuit-bracker-pattern-inside-microservices-254a7a2b48d7"
     },
        {
-        id:2,
+        id:3,
         name:"[Solved] Cross-Site Request Forgery (CSRF) Attacks with Spring Security",
         tags:["Spring","CSRF Attack","CSRF protection","Java","SpringSecurity"],
         date:"8 March, 2024",
@@ -20,7 +28,7 @@ export const Blogs = [
     
   
     {
-        id:3,
+        id:4,
         name:"Synchronizing Spring: The Artistry of Data Transfer Objects (DTOs) for Effortless Communication in Your Boot Applications",
         tags:["Java","SpringBoot","DTO","Spring","DesignPatterns"],
         date:"17 February, 2024",
@@ -28,7 +36,7 @@ export const Blogs = [
         link:"https://medium.com/@lpramithamj/monolithic-vs-soa-vs-microservices-architecture-a-java-perspective-6d3d9fb26ac7"
     },
     {
-        id:4,
+        id:5,
         name:"Monolithic vs. SOA vs. Microservices Architecture: A Java Perspective",
         tags:["Java","Monolithic","SOA","Microservices","Architecture"],
         date:"8 February, 2024",
@@ -36,7 +44,7 @@ export const Blogs = [
         link:"https://medium.com/@lpramithamj/monolithic-vs-soa-vs-microservices-architecture-a-java-perspective-6d3d9fb26ac7"
     },
     {
-        id:5,
+        id:6,
         name:"Art of Clean Code: Understanding the SOLID Principles in Java",
         tags:["Java","OOD","SOLID","DesignPatterns","CleanCode"],
         date:"1 February, 2024",
@@ -44,7 +52,7 @@ export const Blogs = [
         link:"https://medium.com/@lpramithamj/art-of-clean-code-understanding-the-solid-principles-in-java-dcedb42a3f07"
     },
     {
-        id:6,
+        id:7,
         name:"Mastering Spring Data JPA Testing: A Comprehensive Guide to Repository Layer Testing.",
         tags:["Java","Spring","Testing","JPA","Repository","Mockito","JUnit"],
         date:"28 December, 2023",
@@ -52,7 +60,7 @@ export const Blogs = [
         link:"https://medium.com/@lpramithamj/mastering-spring-data-jpa-testing-a-comprehensive-guide-to-repository-layer-testing-f7b1863afa95"
     },
     {
-        id:7,
+        id:8,
         name:"Microservices Unleashed: Elevating Software Development",
         tags:["Java","Spring","Microservices","Architecture","DesignPatterns"],
         date:"11 December, 2023",
@@ -61,7 +69,7 @@ export const Blogs = [
 
     },
      {
-        id:8,
+        id:9,
         name:"The Magic of Git & GitHub",
         tags:["Git","GitHub","VersionControl","SoftwareDevelopment"],
         date:"28 February, 2024",
